0024fcd09cd9035c2a128432518523392158ceba
AdamFinnell <afinnell@perseverenow.org>
2026-03-12 11:09:53 -0500

    projects imgs added

---
+++ projects.js
@@ -0,0 +1,24 @@
+export const projects = [
+
+    {
+        name: "project 1",
+        image: "images/TheQuest.png",
+        link: "https://yourproject.com",
+        description: "the description"
+    },
+
+    {
+        name: "project 2",
+        image: "images/satelliteTracker.png",
+        link: "https://yourproject.com",
+        description: "the description"
+    },
+
+    {
+        name: "project 3",
+        image: "images/EmojiTranslator.png",
+        link: "https://yourproject.com",
+        description: "the description"
+    }
+
+]
