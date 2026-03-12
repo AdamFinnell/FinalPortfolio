8d2a8e5beb7cb855da9395e10b1b9d5bce54be2f
AdamFinnell <afinnell@perseverenow.org>
2026-03-12 12:17:59 -0500

    Theme toggle

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ window.scrollToProjects = () => {
         behavior: "smooth"
     })
 }
+
+const grid = document.getElementById("projectGrid")
+
+projects.forEach(project => {
+
+    const card = document.createElement("a")
+
+    card.href = project.link
+    card.target = "_blank"
+
+    card.className =
+        "group block bg-zinc-900 rounded-xl overflow-hidden hover:scale-105 transition"
+
+    card.innerHTML = `
+    <img src="${project.image}" class="w-full h-60 object-cover">
+
+    <div class="p-6">
+      <h3 class="text-xl font-bold mb-2 group-hover:text-blue-400">
+        ${project.name}
+      </h3>
+
+      <p class="text-gray-400 text-sm">
+        ${project.description}
+      </p>
+    </div>
+  `
+
+    grid.appendChild(card)
+
+})
